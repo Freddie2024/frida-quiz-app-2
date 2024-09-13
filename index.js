0dc12f7a21b81a39f2eb5346fdfec2183000b25b
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-13 13:29:17 +0200

    add if-else-statement for bookmark.png

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 console.log("Hello World!")
 
-const bookmark = document.querySelector("img");
+const bookmark = document.querySelector("#bookmarkIcon");
 bookmark.addEventListener("click", () => {
     console.log("bookmarked");
+    const currentSrc = bookmark.src.split("/").pop();
+    if (currentSrc === "bookmark.png") {
+        bookmark.src = "./assets/bookmark_filled.png";
+    } else {
+        bookmark.src = "./assets/bookmark.png";
+    }
 });
