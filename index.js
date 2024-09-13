798d30459e8c256ba2b27bd866446326974f214c
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-13 15:54:41 +0200

    remove id# and add .question-card__icon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Hello World!")
 
-const bookmark = document.querySelector("#bookmarkIcon");
+const bookmark = document.querySelector(".question-card__icon");
 bookmark.addEventListener("click", () => {
     console.log("bookmarked");
     const currentSrc = bookmark.src.split("/").pop();
