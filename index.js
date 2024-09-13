4ccb3a773ff4b411b12fdaac402d30dd20445171
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-13 17:58:47 +0200

    delete comments

--- index.js
+++ index.js
@@ -23,31 +23,3 @@ answerButton.addEventListener("click", () => {
         answerButton.textContent = "hide answer";
     }
 });
-/*
-
-    answerButton.textContent = "hide answer";
-
-
-    if (hiddenAnswer.display !== "none") {
-        text
-    }
-  });
-*/
-
-  /*
-
-console.log("Yeah");
-
-  var button = document.getElementById('button'); // Assumes element with id='button'
-
-button.onclick = function() {
-    var div = document.getElementById('newpost');
-    if (div.style.display !== 'none') {
-        div.style.display = 'none';
-    }
-    else {
-        div.style.display = 'block';
-    }
-};
-
-*/
