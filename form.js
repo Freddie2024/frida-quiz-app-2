adfe4051fa9ca22e98a64fea078f2ab18bf7b444
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-14 19:04:13 +0200

    add variable formElements and console.log question, answer, tag

--- form.js
+++ form.js
@@ -1,8 +1,10 @@
 const form = document.querySelector('[data-js="form"]');
 
-console.log("It works!")
-
-form.addEventListener("submit", (event)) => {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
-};
+    const formElements = event.target.elements;
+    console.log(formElements.yourQuestion.value);
+    console.log(formElements.yourAnswer.value);
+    console.log(formElements.tag.value);
+});
 
