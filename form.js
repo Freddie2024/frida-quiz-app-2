b4410f91928b4db2c571aa0b7e7b9e9072fdd003
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-15 16:30:38 +0200

    delete class input-field in html, add eventListener for question-input characters left

--- form.js
+++ form.js
@@ -1,19 +1,36 @@
+
 const questionInput = document.querySelector('[data-js="yourQuestion"]');
-
-
+const answerInput = document.querySelector('[data-js="yourAnswer"]');
 
 const charactersLeftElement = document.querySelector(
     '[data-js="remaining-characters"]'
   );
 
+  questionInput.addEventListener("input", (event) => {
+  charactersLeftElement.textContent = 150 - parseInt(event.target.value.length);
+});
 
-questionInput.addEventListener("input", (event) => {
-      charactersLeftElement.textContent = 150 - parseInt(event.target.value.length);
-  });
+/*
+  const charactersLeftElement2 = document.querySelector(
+    '[data-js="remaining-characters2"]'
+  );
+ */
+
+/*
+  document.querySelectorAll('input-field').forEach(item => {
+    item.addEventListener("input", (event) => {
+    charactersLeftElement.textContent = 150 - parseInt(event.target.value.length);
+    }
+});
+*/
+
+/*
+  answerInput.addEventListener("input", (event) => {
+    charactersLeftElement2.textContent = 150 - parseInt(event.target.value.length);
+});
+*/
 
 const form = document.querySelector('[data-js="form"]');
-
-//  const answerInput = document.querySelector('[data-js="yourAnswer"]');
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
