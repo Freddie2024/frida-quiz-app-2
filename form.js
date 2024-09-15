0f9fe7c59f189ca4282a837ec2149e26920c9ede
FRIDA LEMKE <frida.lemke@gmx.de>
2024-09-15 12:44:11 +0200

    add variables for card-elements to createElemtents, add content and append them

--- form.js
+++ form.js
@@ -8,6 +8,20 @@ form.addEventListener("submit", (event) => {
     console.log(formElements.tag.value);
 
     const section = document.createElement("section");
-    document.form.append(section);
+    const img = document.createElement("img");
+    img.src = "./assets/bookmark.png"
+    const h2 = document.createElement("h2");
+    h2.textContent = "Your question displayed";
+    const button = document.createElement("button");
+    button.textContent = "Show answer";
+    const p = document.createElement("p");
+    p.textContent = "Your answer displayed";
+    const div = document.createElement("div");
+    div.textContent = "Your chosen tag";
+
+   
+    section.append(img, h2, button, p, div);
+   
+    form.append(section);
 });
 
